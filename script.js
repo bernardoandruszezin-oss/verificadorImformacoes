0832c617b4901280913974eb69c5a6ecdae0d982
bernardoandruszezin-oss <bernardo.andruszezin@escola.pr.gov.br>
2026-08-06 10:42:55 -0300

    ok

---
+++ script.js
@@ -0,0 +1,15 @@
+//botoes
+
+const bntContraste = document.getElementById("bnt-contraste");
+const bntAumentar = document.getElementById("bnt-aumentar-texto");
+const bntDiminuir = document.getElementById("bnt-diminuir-texto");
+
+//Mudar tamanho da fonte
+
+let tamanhoAtualFonte = 100;
+
+//Alto contraste
+
+bntContraste.addEventListenter("click", ()=>{
+    document.body.clasList.
+})
